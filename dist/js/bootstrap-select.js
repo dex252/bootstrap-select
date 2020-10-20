c8e2c103c14b0348f53ecbda08561a3da81a9ad3
Slava <38129962+dex252@users.noreply.github.com>
2020-10-20 21:29:53 +1000

    Update bootstrap-select.js

--- dist/js/bootstrap-select.js
+++ dist/js/bootstrap-select.js
@@ -660,7 +660,7 @@
   var classNames = {
     DISABLED: 'disabled',
     DIVIDER: 'divider',
-    SHOW: 'open',
+    SHOW: 'show',
     DROPUP: 'dropup',
     MENU: 'dropdown-menu',
     MENURIGHT: 'dropdown-menu-right',
